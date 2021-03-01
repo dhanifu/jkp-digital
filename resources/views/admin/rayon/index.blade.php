02c95a65f32375275e2859142de7642538e7d4fb
Agung prasetio <agungprasetio878@gmail.com>
2021-03-02 00:11:14 +0700

    crud rayons tapi error

--- resources/views/admin/rayon/index.blade.php
+++ resources/views/admin/rayon/index.blade.php
@@ -1,11 +1,7 @@
 @extends('_layouts.app')
 
 @section('content')
-    <div class="container">
-    <div class="row justify-content-center">
-        <livewire:rayon.create>
-        <livewire:rayon.edit>
-    </div>
+<div class="container">
     <div class="row justify-content-center">
         <livewire:rayon.data>
     </div>
--- resources/views/livewire/rayon/create.blade.php
+++ resources/views/livewire/rayon/create.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="card shadow">
+    <div class="card-header py-3">
+        <h2 class="card-title h6 font-weight-bold text-primary m-0">Tambah Rayon</h2>
+    </div>
+    <div class="card-body">
+        <form wire:submit.prevent="store">
+            <div class="form-group">
+                <label>Rayon</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" wire:model="name"
+                    placeholder="Nama" autofocus>
+
+                @error('name')
+                <span class="invalid-feedback">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label>Pembimbing</label>
+                <select style="width: 100%" class="form-control  @error('pembimbing_id') is-invalid @enderror" wire:model="pembimbing_id">
+                    <option>-- Select --</option>
+                    @foreach ($pembimbings as $pembimbing)
+                    <option value="{{ $pembimbing->id }}">{{ $pembimbing->name }}</option>
+                    @endforeach
+                </select>
+
+                @error('pembimbing_id')
+                <span class="invalid-feedback">{{ $message }}</span>
+                @enderror
+            </div>
+            <button class="btn btn-primary shadow" type="submit">Tambah</button>
+        </form>
+    </div>
 </div>
