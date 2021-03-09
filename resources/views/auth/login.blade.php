c44622e614d582edf7e241a039a113f48bfe79f9
Dhievha03 <aryadhievha03@gmail.com>
2021-03-10 00:45:28 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,60 @@
-@extends('_layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/tailwind.css')}}">
+    <title>Login</title>
+</head>
+    <style>
+        .split{
+            background-image: url({{asset('image/wikrama.png')}})
+        }
+    </style>
+<body>
+    <div class="grid grid-cols-1 w-full lg:grid-cols-2 h-screen">
+        <div class="split bg-cover w-full hidden lg:block">
+            <div class="layer"></div>
+            
+        </div>
+        <div class="w-full bg-gray-100">
+            <div class="right flex justify-center items-center h-screen">
+                <div class="bg-gray-100 p-8 sm:w-96 w-80 rounded-sm">
+                    <div class="text-center text-2xl lg:text-3xl font-semibold md:text-2xl">
+                        <h1>JKP Digital</h1>
+                    </div>
+                    <form action="{{ route('login') }}" method="POST">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mt-2">
+                            <label for="" class="block">Email</label>
+                            <input type="email" name="email" id="email" class="w-full py-1 md:px-2 px-1 border-2 rounded box" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="text-red-600 text-xs" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mt-2">
+                            <label for="" class="block">Password</label>
+                            <input type="password" name="password" id="password" class="w-full py-1 md:px-2 px-1 border-2 rounded focus:outline-none box" required autocomplete="current-password">
+                            @error('password')
+                                <span class="text-red-600 text-xs" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <label class="inline-flex items-center mt-3">
+                            <input type="checkbox" class="form-checkbox h-4 w-4 text-blue-500"><span class="ml-2 text-black text-sm">Remember me</span>
+                        </label>
+    
+                        <div>
+                            <button type="submit" class="w-full mt-4 bg-blue-700 md:py-2 py-1 text-white font-bold rounded focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50">Masuk</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
