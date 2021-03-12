780e93bf4d8ef2ef64bcd4ae19317b54f5f91de0
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-03-12 19:07:09 +0700

    sidebar active assignments

--- resources/views/_partials/sidebar.blade.php
+++ resources/views/_partials/sidebar.blade.php
@@ -75,7 +75,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('student.assignment.index') }}" class="{{ request()->is('assignments')? ' active' : '' }}">
+                        <a href="{{ route('student.assignment.index') }}" class="{{ request()->is('assignments/*')? ' active' : '' }}">
                             <span class="icon{{ request()->is('assignments')? ' active' : '' }}">
                                 <i class="fas fa-tasks"></i>
                             </span>
