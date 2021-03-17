9b7db14bbf4d481d8113de9c71910b7ce86802fe
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-03-18 01:31:32 +0700

    load more feature

--- app/Http/Livewire/Student/Assignment/Data.php
+++ app/Http/Livewire/Student/Assignment/Data.php
@@ -7,10 +7,25 @@ use Livewire\Component;
 
 class Data extends Component
 {
+    public $perPage = 6;
+    protected $listeners = [
+        'load-more' => 'loadMore',
+    ];
+
+    public function default()
+    {
+        $this->perPage = 8;
+    }
+
+    public function loadMore()
+    {
+        $this->perPage += 6;
+    }
+
     public function render()
     {
         $assignments = Assignment::select(['id', 'minggu_ke', 'from_date', 'to_date', 'created_at'])
-            ->latest()->get();
+            ->latest()->paginate($this->perPage);
 
         return view('livewire.student.assignment.data', compact('assignments'));
     }
