29daa1ded0c37065efd64447e72caa81a7759cc3
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-03-19 08:48:00 +0700

    load-more

--- app/Http/Livewire/Student/Todo/Assigned.php
+++ app/Http/Livewire/Student/Todo/Assigned.php
@@ -3,16 +3,28 @@
 namespace App\Http\Livewire\Student\Todo;
 
 use App\Models\Assignment;
+use App\Models\Jkp;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Assigned extends Component
 {
-    protected $listeners = ['refreshAssigned' => '$refresh'];
+    public $perPage = 8;
+
+    protected $listeners = [
+        'refreshAssigned' => '$refresh',
+        'load-more' => 'loadMore'
+    ];
+
+    public function loadMore()
+    {
+        $this->perPage += 8;
+    }
 
     public function render()
     {
         $assignments = Assignment::select(['id', 'minggu_ke', 'from_date', 'to_date', 'created_at'])
-            ->latest()->get();
+            ->latest()->paginate($this->perPage);
         return view('livewire.student.todo.assigned', compact('assignments'));
     }
 }
