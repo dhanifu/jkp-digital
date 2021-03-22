810e7f0995134317e0e36c0dcf9c59df9782b431
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-03-23 00:54:42 +0700

    hapus komen

--- app/Http/Livewire/Assignment/Create.php
+++ app/Http/Livewire/Assignment/Create.php
@@ -12,11 +12,11 @@ class Create extends Component
 {
     public $minggu_ke, $from_date, $to_date;
 
-    // protected $rules = [
-    //     'minggu_ke' => 'required',
-    //     'from_date' => 'required|date',
-    //     'to_date' => 'required|date'
-    // ];
+    protected $rules = [
+        'minggu_ke' => 'required',
+        'from_date' => 'required|date',
+        'to_date' => 'required|date'
+    ];
 
     public function store()
     {
@@ -26,30 +26,30 @@ class Create extends Component
          *  dan komen semua yang ada dibawah 2 variable ini
          *  kecuali yang assignment::create()
          */
-        $assignment_terbaru = Assignment::latest()->first();
-        $data = [
-            'minggu_ke' => $assignment_terbaru->minggu_ke + 1,
-            'from_date' => tambahTujuhHari($assignment_terbaru->from_date),
-            'to_date' => tambahTujuhHari($assignment_terbaru->to_date),
-        ];
+        // $assignment_terbaru = Assignment::latest()->first();
+        // $data = [
+        //     'minggu_ke' => $assignment_terbaru->minggu_ke + 1,
+        //     'from_date' => tambahTujuhHari($assignment_terbaru->from_date),
+        //     'to_date' => tambahTujuhHari($assignment_terbaru->to_date),
+        // ];
 
-        // $data = $this->validate();
-        // $data["from_date"] = date('Y-m-d H:i:00', strtotime($data["from_date"]));
-        // $data["to_date"] = date('Y-m-d H:i:59', strtotime($data["to_date"]));
+        $data = $this->validate();
+        $data["from_date"] = date('Y-m-d H:i:00', strtotime($data["from_date"]));
+        $data["to_date"] = date('Y-m-d H:i:59', strtotime($data["to_date"]));
 
         $assignment = Assignment::create($data);
 
-        // $users = DB::table('users')
-        //     ->join('students', 'users.id', '=', 'students.user_id')
-        //     ->select('users.email')
-        //     ->get();
-
-        // foreach ($users as $key => $user) {
-        //     Mail::to($user->email)->send(new AssignmentMail($assignment));
-        // }
-
         $this->reset(['minggu_ke', 'from_date', 'to_date']);
         $this->emit('refresh', 'Sukses Membuat Assignment');
+
+        $users = DB::table('users')
+            ->join('students', 'users.id', '=', 'students.user_id')
+            ->select('users.email')
+            ->get();
+
+        foreach ($users as $user) {
+            Mail::to($user->email)->send(new AssignmentMail($assignment));
+        }
     }
 
     public function render()
