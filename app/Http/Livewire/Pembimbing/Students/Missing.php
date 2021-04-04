4074d26235587c2f4e27defb8831c9a76dd597e1
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-04-05 00:54:44 +0700

    siswa rayon yang missing

--- app/Http/Livewire/Pembimbing/Students/Missing.php
+++ app/Http/Livewire/Pembimbing/Students/Missing.php
@@ -2,12 +2,78 @@
 
 namespace App\Http\Livewire\Pembimbing\Students;
 
+use App\Models\Assignment;
+use App\Models\AssignmentJkp;
+use App\Models\Jkp;
+use App\Models\Rayon;
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Missing extends Component
 {
+    public $minggu_ke;
+    public $perPage = 10;
+    public $weeks;
+
+    protected $listeners = [
+        'refreshMissing' => '$refresh',
+        'load-more' => 'loadMore'
+    ];
+
+    public function loadMore()
+    {
+        $this->perPage += 10;
+    }
+
     public function render()
     {
-        return view('livewire.pembimbing.students.missing');
+        $minggu_ke = $this->minggu_ke;
+        $current_date = strtotime(date('Y-m-d H:i:s'));
+
+        if ($minggu_ke == null) {
+            $minggu_ke = $this->weeks[0]->minggu_ke;
+        }
+
+        $pemray = Auth::user()->teacher;
+        $rayon = Rayon::where('teacher_id', $pemray->id)->first();
+
+        $students = Student::where('rayon_id', $rayon->id)
+            ->with('akun:id,email,pemilik_id', 'rombel')->orderBy('name', 'ASC')->get();
+
+        $jkp_dones = Jkp::with('assignment')->whereHas('assignment', function ($q) use ($minggu_ke) {
+            $q->where('minggu_ke', $minggu_ke);
+        })->latest()->paginate($this->perPage);
+
+        $done = [];
+        $user_id = [];
+        $jkp_user_id = [];
+
+        foreach ($jkp_dones as $key => $item) {
+            foreach ($students as $index => $value) {
+                $user_id[$index] = $value->akun->id;
+                $jkp_user_id[$key] = $item->user_id;
+
+                $values = $value->toArray();
+
+                $done[] = $jkp_user_id[$key] == $user_id[$index] ? $values : [];
+            }
+        }
+
+        $dones = array_filter($done);
+        $missings = $students->toArray();
+
+        if (!empty($dones)) {
+            foreach ($missings as $key => $siswa) {
+                foreach ($dones as $selesai) {
+                    if ($siswa["akun"]["id"] == $selesai["akun"]["id"]) {
+                        unset($missings[$key]);
+                    }
+                }
+            }
+        }
+
+        return view('livewire.pembimbing.students.missing', compact('missings'));
     }
 }
--- resources/views/livewire/pembimbing/students/missing.blade.php
+++ resources/views/livewire/pembimbing/students/missing.blade.php
@@ -1,3 +1,58 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="float-left">
+        <button wire:click="$refresh" class="btn btn-primary">Refresh</button>
+    </div>
+
+    <div class="float-right">
+        <select wire:model=minggu_ke id="minggu_ke" class="form-control" style="width: 200px">
+            @foreach ($weeks as $w)
+                <option value="{{ $w->minggu_ke }}">
+                    Minggu Ke {{ $w->minggu_ke }}
+                </option>
+            @endforeach
+        </select>
+
+        {{-- <div class="btn-group-vertical">
+            <button class="btn btn-success ml-2" type="button">
+                Excel &nbsp;&nbsp; <i class="far fa-file-excel"></i>
+            </button>
+        </div> --}}
+    </div>
+
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered mt-4">
+            <thead>
+                <tr>
+                    <th style="width: 5px">#</th>
+                    <th>Nama</th>
+                    <th>NIS</th>
+                    <th>Rombel</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($missings as $s)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $s['name'] }}</td>
+                        <td>{{ $s['nis'] }}</td>
+                        <td>{{ $s['rombel']['name'] }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="2" align="center">Kosong</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </div>
+
+@push('script')
+    <script>
+        window.onscroll = function(ev) {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                window.livewire.emit('load-more');
+            }
+        };
+    </script>
+@endpush
