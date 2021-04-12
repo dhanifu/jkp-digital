ef29c1edcc8719f4f67b3430862d9f4acb3b59d6
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-04-13 00:24:14 +0700

    dihapus karena mau diganti

--- resources/views/kesiswaan/literasi/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('_layouts.app')
-@section('title' , 'Literasi')
-@section('content')
-    <div class="justify-content-center">
-        <livewire:kesiswaan.literasi.index>
-    </div>
-@endsection
