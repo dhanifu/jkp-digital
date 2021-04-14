5b681132b8a93d620b00e3d7af7ef81be81b4c2e
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-04-15 03:01:08 +0700

    ubah title, passing data dari url

--- resources/views/kesiswaan/student.blade.php
+++ resources/views/kesiswaan/student.blade.php
@@ -1,8 +1,11 @@
 @extends('_layouts.app')
-@section('title' , getRayonById(request()->rayon)->name)
+@section('title' , ucwords(request()->jenisJkp) . " - " . getRayonById(request()->rayon)->name)
 
 @section('content')
     <div class="justify-content-center">
-        @livewire('kesiswaan.student.tab', ['rayon_id' => request()->rayon])
+        @livewire('kesiswaan.student.tab', [
+            'jenisJkp' => request()->jenisJkp,
+            'rayon_id' => request()->rayon
+        ])
     </div>
 @endsection
