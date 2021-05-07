3fee0893437b48b41318eff68d0a98d9c929e20f
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-05-07 21:33:49 +0700

    mindahin css & script

--- resources/views/kesiswaan/student.blade.php
+++ resources/views/kesiswaan/student.blade.php
@@ -8,3 +8,46 @@
         ])
     </div>
 @endsection
+
+@push('css')
+    <style>
+        .detail-img {
+            object-fit: none; /* Do not scale the image */
+            object-position: center; /* Center the image within the element */
+            width: 8rem;
+            max-height: 6rem;
+        }
+        @media only screen and (max-width:767px){
+            .detail-img {
+                width: 100%;
+            }
+            .teks {
+                margin-top: 15px;
+            }
+        }
+        @media only screen and (max-width:760px){
+            .dropdown {
+                margin-top: 10px;
+            }
+        }
+    </style>
+@endpush
+
+@push('script')
+    <script>
+        $(".dropdown-menu").click(function (e) {
+            e.stopPropagation()
+        })
+
+        $("#download-excel").click(function(){
+            let query = {
+                minggu_ke: $("#minggu_ke").val()
+            }
+
+            let url = '{{ route('kesiswaan.export-excel', ':id') }}'
+            link = url.replace(':id', '{{ request()->rayon }}') + "?" + $.param(query)
+
+            window.location = link
+        })
+    </script>
+@endpush
