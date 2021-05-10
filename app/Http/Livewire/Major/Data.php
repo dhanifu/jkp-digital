28a122af77701056b8e6048ea637d895ec4263c8
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-05-11 00:45:34 +0700

    fitur search

--- app/Http/Livewire/Major/Data.php
+++ app/Http/Livewire/Major/Data.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Livewire\Major;
+
 use App\Models\Major;
 use Livewire\WithPagination;
 use Livewire\Component;
@@ -9,6 +10,7 @@ class Data extends Component
 {
     use WithPagination;
 
+    public $search = null;
     protected $paginationTheme = 'bootstrap';
     protected $listeners = ['refresh', 'delete'];
 
@@ -21,12 +23,18 @@ class Data extends Component
 
     public function refresh(string $message)
     {
+        $this->search = null;
         session()->flash('success', $message);
     }
 
-    public function render(Major $major)
+    public function render()
     {
-        $majors = $major->latest()->paginate(5);
+        $search = $this->search;
+
+        $majors = Major::select('id', 'name')
+            ->where('name', 'like', "%$search%")
+            ->latest()->paginate(5);
+
         return view('livewire.major.data', compact('majors'));
     }
 }
