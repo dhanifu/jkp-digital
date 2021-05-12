dcc7012495871782d2850af7c7232d46789dee71
Dhievha03 <aryadhievha03@gmail.com>
2021-05-12 08:00:57 +0700

    footer

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -3,21 +3,26 @@
 <head>
     @include('_includes.head')
 </head>
-<body>
-    <div class="wrapper">
-        @include('_partials.sidebar')
-        @include('_partials.navbar')        
-    </div>
+<body class="h-full relative">
 
-    <div class="main-container">
-        <div class="z-10 w-full py-4 px-1 md:p-6">
-            @yield('content')
+    <div class="min-h-screen block relative pb-8">
+        <div class="wrapper">
+            @include('_partials.sidebar')
+            @include('_partials.navbar')        
         </div>
+    
+        <div class="main-container min-h-full">
+            <div class="z-10 w-full py-4 px-1 md:p-6">
+                @yield('content')
+            </div>
+        </div>
+
+        @include('_partials.footer')
     </div>
+    
     
     @yield('modal')
 
-    @include('_partials.footer')
 
     @include('_includes.foot')
 </body>
